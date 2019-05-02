36fb4cd1c15a3fac5bc336bf5b3696cbb4d34e32
Rob Farmer <rofarmer@chapman.edu>
2019-05-02 20:46:19 +0000

    Added choices 1-7. Need code for each.

--- main.cpp
+++ main.cpp
@@ -13,41 +13,76 @@ int choice = 0;
 
 int int main(int argc, char const *argv[])
 {
-  return 0;
-
+  //Check for existence of facultyTable and studentTable here
   while (true)
   {
     if (choice == 1)
     {
       //Print all students and their info (sorted ascending)
+      //1. iterate through (STUDENT) tree using INORDER TRAVERSAL
+      //2. return student information after the lookup
+      //3. print to screen
     }
     if (choice == 2)
     {
-      //Print all students and their info (sorted ascending)
+      //Print all faculty and their info (sorted ascending)
+      //1. iterate through (FACULTY) tree using INORDER TRAVERSAL
+      //1.1 We can reuse the function from option 1 here.
+      //2. return student information after the lookup
+      //3. print to screen
     }
     if (choice == 3)
     {
-      //Print all students and their info (sorted ascending)
+      //Find and display student info given the FACULTY ID
+      //1. use search function on the (STUDENT) binary tree, using ID number and tree as the function arguments
+      //2. return student info based on ID after navigating.
+      //3. choice should have a runtime of O(log(n))
+      //4. print to screen
     }
     if (choice == 4)
     {
-      //Print all students and their info (sorted ascending)
+      //Find and display faculty info given the FACULTY ID
+      //1. use search function on the (FACULTY) binary tree, using ID number and tree as the function arguments
+      //1.1 reusing the function from option 3 here.
+      //2. return faculty info based on ID after navigating
+      //3. choice should have a runtime of O(log(n))
+      //4. print to screen
     }
+
     if (choice == 5)
     {
-      //Print all students and their info (sorted ascending)
+      //Given a student ID, print name and info of Faculty advisor
+      //1. use search function on the (STUDENT) BST, using ID number and tree as function arguments.
+      //2. return the student's faculty advisor ID number.
+      //3. use search function on the (FACULTY) BST, using ID number and tree as function arguments.
+      //4. return faculty info, including name and ID number.
+      //5. print to screen.
+      //6. choice should have a runtime of O(2log(n)) due to double search.
     }
+
     if (choice == 6)
     {
-      //Print all students and their info (sorted ascending)
+      //pre-step: create queue (student_ID_queue of type int) to store student ID numbers (to be listed later).
+      //1. use search function on the (FACULTY) BST, using ID number and tree as function arguments.
+      //2. enqueue each student ID number (not the student!) listed in faculty object (found in step 1)
+      //3. use search function on the (STUDENT) BST, using ID number* and tree as function arguments.
+      //3.1 return student info
+      //3.2 print to screen upon returned student
+      //3.3 dequeue each ID number after it has been searched and returned
+      //4. choice should have a runtime of O(xlog(n)) with x representing number of students listed by advisor
+      //5. delete student_ID_queue
+      //6. print deletion confirmation
     }
     if (choice == 7)
     {
-      //Print all students and their info (sorted ascending)
+      //pre-step: create object of type student, given their ID, name, level, major, GPA, and advisor
+      //1. Add student to tree
+      //2. OPTIONAL: re-balance tree (lol that ain't happening)
+      //3. print upon success or failure.
     }
     if (choice == 8)
     {
-      //Print all students and their info (sorted ascending)
+      //
     }
     if (choice == 9)
     {
@@ -75,4 +110,5 @@ int int main(int argc, char const *argv[])
       //System Exit
     }
   }
+  return 0;
 }
