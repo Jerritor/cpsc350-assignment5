fd9caa375bd062180ab0f22f882b65a0b7a15786
Jerritor <jambtor@gmail.com>
2019-05-02 17:53:00 +0000

    uploaded empty studentbst implementation

---
+++ StudentBST.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+
+using namespace std;
+
+///////STUDENT BST//////
+StudentBST::StudentBST()
+{
+
+}
+
+StudentBST::~StudentBST()
+{
+
+}
+
+void StudentBST::insert(StudentNode s)
+{
+
+}
+
+void StudentBST::printTree()
+{
+
+}
+
+void StudentBST::serializeTree()
+{
+
+}
+
+StudentNode StudentBST::findID(int id)
+{
+
+}
+
+void StudentBST::removeStudent(StudentNode s)
+{
+
+}
+
+void StudentBST::updateAdvisor(int student, int faculty)
+{
+
+}
+
+int StudentBST::size()
+{
+
+}
+
+bool StudentBST::isEmpty()
+{
+
+}
+
+///////FACULTY BST//////////////
+
+FacultyBST::FacultyBST()
+{
+
+}
+
+FacultyBST::~FacultyBST()
+{
+
+}
+
+void FacultyBST::insert(StudentNode s)
+{
+
+}
+
+void FacultyBST::printTree()
+{
+
+}
+
+void FacultyBST::serializeTree()
+{
+
+}
+
+StudentNode FacultyBST::findID(int id)
+{
+
+}
+
+void FacultyBST::removeStudent(StudentNode s)
+{
+
+}
+
+void FacultyBST::removeAdvisees(int fac)
+{
+
+}
+
+int FacultyBST::size()
+{
+
+}
+
+bool FacultyBST::isEmpty()
+{
+
+}
